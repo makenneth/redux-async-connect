64347fd1b7bf6db92f2e2a829bff13c1b4a8d929
Kenneth Ma <kenneth.ma@motionloft.com>
2018-07-28 18:05:26 -0700

    update

--- modules/ReduxAsyncConnect.js
+++ modules/ReduxAsyncConnect.js
@@ -3,7 +3,7 @@ import RouterContext from 'react-router/lib/RouterContext';
 import { beginGlobalLoad, endGlobalLoad } from './asyncConnect';
 import { connect } from 'react-redux';
 
-const { array, func, object, any } = React.PropTypes;
+import { array, func, object, any } from 'prop-types';
 
 /**
  * We need to iterate over all components for specified routes.
